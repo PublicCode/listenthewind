6da21965b74577a68beda92e734bb29851f6251e
Peter Sun <Peter.Sun@t2vsoft.com>
2014-11-25 22:29:46 +0800

    Fix complicit

--- ListenTheWindWeb/Scripts/T2VBusiness/Camp/CampBook.js
+++ ListenTheWindWeb/Scripts/T2VBusiness/Camp/CampBook.js
@@ -1,1 +1,7 @@
-﻿
+﻿soNgModule.controller("CampBook", ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
+
+    if (t2v_CampDetail.CampInfo) {
+        $scope.camp = JSON.parse(t2v_CampDetail.CampInfo);
+    }
+
+}]);
