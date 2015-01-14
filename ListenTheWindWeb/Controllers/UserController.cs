657cfa10d753e9bc3eafff114f77264cd5c303e2
cuiguoqiang <guoqiang.cui@t2vsoft.com>
2015-01-14 11:12:32 +0800

    add user page

---
+++ ListenTheWindWeb/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using T2VSoft.MVC.Core;
+
+namespace HDS.QMS.Controllers
+{
+    public class UserController : T2VController
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UserPayBook()
+        {
+            return View();
+        }
+
+    }
+}
