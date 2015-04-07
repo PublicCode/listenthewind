45a168a0e317dd95ad288b552d9eb0a3256aee11
liehuo224 <liehuo224@163.com>
2015-04-07 23:00:39 +0800

    add approval camp list.

---
+++ ListenTheWindWeb/Controllers/CampApprovalController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HDS.QMS.Controllers
+{
+    public class CampApprovalController : Controller
+    {
+        //
+        // GET: /CampApproval/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
